49a0fd3cba42aed70989731c795adfdcbcf7e206
David Stroch <d.stroch@gmail.com>
2020-12-08 10:28:28 +0100

    seventh day

--- 2020/07/D07_02.py
+++ 2020/07/D07_02.py
@@ -2,7 +2,7 @@ import re
 
 pattern = re.compile(r"[1-9]{1} \w+ \w+")
 
-text = """shiny gold bags contain 2 dark red bags.
+text2 = """shiny gold bags contain 2 dark red bags.
 dark red bags contain 2 dark orange bags.
 dark orange bags contain 2 dark yellow bags.
 dark yellow bags contain 2 dark green bags.
@@ -11,11 +11,22 @@ dark blue bags contain 2 dark violet bags.
 dark violet bags contain no other bags.
 """
 
+text1 = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+"""
+
 
 with open('input') as f:
      page = f.read().strip().splitlines()
 
-# page = text.strip().splitlines()
+# page = text2.strip().splitlines()
 page = [line.split(' bags contain ') for line in page]
 
 found_colors = set()
