696818d1660fcf7181bcae7d8f0446b7a9a38c5b
David Stroch <d.stroch@gmail.com>
2020-12-10 21:33:43 +0100

    8th day part one update

--- 2020/08/D08_1.py
+++ 2020/08/D08_1.py
@@ -1,3 +1,5 @@
+import sys
+
 text_test = """nop +0
 acc +1
 jmp +4
@@ -8,35 +10,52 @@ acc +1
 jmp -4
 acc +6
 """
-with open('input') as f:
-     INSTRUCTIONS = f.read().strip().splitlines()
-# INSTRUCTIONS = text_test.strip().splitlines()
 
 
-def get_instruction(index):
-    return INSTRUCTIONS[index].split(' ')
+def format_input(input_string):
+    lines = input_string.splitlines()
+
+    original_instructions = []
+    for line in lines:
+        command, value = line.split()
+        original_instructions.append((command, int(value)))
+
+    return original_instructions
 
 
-def main():
 
+def main(instructions):
     accumulate = 0
-    position = 0
+    index = 0
     visited = []
 
-    while position not in visited:
-        command, value = get_instruction(position)
-        visited.append(position)
+    while index not in visited:
+        try:
+            command, value = instructions[index]
+        except IndexError:
+            if index < 0:
+                return 'mensi index nez 0'
+            else:
+                return 'vetsi index nez 0'
+
+        visited.append(index)
 
         if command == 'acc':
-            accumulate += int(value)
-            position += 1
+            accumulate += value
+            index += 1
         elif command == 'jmp':
-            position += int(value)
+            index += value
+        elif command == 'nop':
+            index += 1
         else:
-            position += 1
+            raise RuntimeError("I cannot resolve this command")
 
     return accumulate
 
 
-print(main())
+with open('input') as f:
+     input_string = f.read()
+
+original_instructions = format_input(input_string)
+print(main(original_instructions))
 
